6e4eea600d22833dcfd7724c6a81f84d2e9d4ac5
EduardoCopat <ecopat@gmail.com>
2016-07-25 20:02:40 -0300

    Enables user to properly select product

--- resources/public/salesOrder/createSalesOrderController.js
+++ resources/public/salesOrder/createSalesOrderController.js
@@ -1,17 +1,26 @@
 angular.module('webStore').controller('createSalesOrderController', function ($scope, $http, $state) {
-    $scope.form = {productId: '', quantity: 0, customerName: ''};
+    var emptyForm =  {productId: '', quantity: 0, customerName: ''};
+    $scope.form = emptyForm;
+    $scope.selectedProduct = '';
     $scope.messages = {form: '',productId: '', quantity: '', customerName: ''};
     $scope.status = {productId: '', quantity: '', customerName: ''};
     $scope.submit = function () {
         $scope.messages.form = 'Creating sales order...';
+        $scope.form.productId = $scope.selectedProduct.id;
         $http.post('/sales-order', $scope.form).then(function (response) {
             $scope.messages.form = '';
-            if (response.data.success)
-                //$state.go('webStore.products');
-                window.alert("Sales order created");
+            if (response.data.success){
+                $scope.messages.form = "Sales order created";
+                $scope.form = emptyForm;
+            }
             else {
                 window.alert("Error");
             }
         });
     };
+    $http.get('/products').then(function (response) {
+        if (response.data.length == 0)
+            window.alert("you need to have at least 1 product to create a sales order");
+        $scope.products = response.data;
+    });
 });
